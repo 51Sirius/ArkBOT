82cc06123e1a2225e9ffe9373e132966b9822ff7
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-04-14 23:49:27 +0300

    Welcome message

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ intents.typing = False
 intents.presences = False
 intents.members = True
 client = commands.Bot(command_prefix="!", intents=intents)
+
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined to server')
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(cfg.WEBHOOK_URL, adapter=AsyncWebhookAdapter(session))
+        image = member.avatar
+        await webhook.send(f'Hello {member} and welcome to our server', username='Welcome')
